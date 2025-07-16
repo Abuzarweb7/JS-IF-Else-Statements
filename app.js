b850d663437b27365dfce7c0978351780d700cb1
hammad-sohail <hammadsohail733@gmail.com>
2025-07-16 13:49:49 +0500

    q14

---
+++ app.js
@@ -0,0 +1,112 @@
+// Q1
+
+// var age =prompt("Enter your age");
+// if(age>=18){
+//     alert("Your are adult");
+// }
+// else{
+//     alert("Your are a minor");
+//  }
+// Q2
+// var num =1;
+// if(num>0){
+//     console.log("Positive num");
+// }
+// else{
+//     console.log("Not positive");
+// }
+// Q3
+// var num =prompt("Enter your num");
+// if(num%2===0){
+//     alert("Even Number");
+// }
+// else{
+//     alert("Odd Number");
+// }
+//Q4
+// var admin =prompt("Enter your username");
+// if(admin==="admin"){
+//     alert("Welcome admin");
+// }
+// else{
+//     alert("Access denied"); 
+// }
+//Q5
+// var num1 = prompt("Ënter first num");
+// var num2 =prompt("Ënter your second num");
+// if(num1===num2){
+//     console.log("Numbers are same");
+// }
+// else{
+//     console.log("Numbers are not same");
+// }
+// Q6
+// var num =prompt("Enter first num");
+// var num2 =prompt("Enter second num");
+
+
+// if(num>num2){
+//     alert("First is larger");
+// }
+// else{
+//     alert("Second is larger");
+// }
+// Q7
+// var enterNum =prompt("Enter your Num");
+// if(enterNum>=100){
+//     alert("TO big");
+// }
+// else{
+//     alert("Within range");
+// }
+//Q8
+// var score =prompt("Enter your score");
+// if(score>=50){
+//     console.log("passed");
+// }
+// else{
+//     console.log("failed");
+// }
+//Q9
+// var temperature =prompt("Enter your temperature");
+// if(temperature>=30){
+// alert("Very hot");
+
+// }
+// else{
+//     alert("Not hot");
+// }
+//Q10
+// var enterZero =prompt("Enter zero num");
+// if(enterZero<=0){
+//     alert("Zero");
+
+// }
+// else{
+//     alert("Not zero");
+// }
+// Q11
+// var age = prompt("Enter your age");
+// if(age>=60){
+//     console.log("You are a senior citizen");
+
+// }
+// else{
+//     console.log("You are not a senior");
+// }
+// Q12
+// var nameAlice=prompt("Enter your name");
+// if(nameAlice==="Alice"){
+//     alert("Hi! Alice");
+// }
+// else{
+//     alert("You're not Alice!");
+// }
+// Q13
+// var enterPassword =prompt("Enter your password");
+// if(enterPassword==="12345"){
+//     console.log("Correct password");
+// }
+// else{
+//     console.log("Wrong password");
+// }
